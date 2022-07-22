e9c971486d43740d4f5f220bd24f12d054d67f01
anirudh797 <anirudh.kaptiyal77@gmail.com>
2022-07-22 23:36:14 +0530

    July 22 - Refactored Adapter for spinner_list_item.xml for Categories List

--- app/src/main/java/com/example/moneytracker/common/CustomDialog.kt
+++ app/src/main/java/com/example/moneytracker/common/CustomDialog.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.DialogFragment
 import com.example.moneytracker.R
 import com.example.moneytracker.databinding.AddIncomeExpenseDialogBinding
@@ -34,6 +35,33 @@ class CustomDialog : DialogFragment() {
             spinner.adapter = CustomAdapter(requireContext(),R.layout.spinner_list_item,
                 resources.getStringArray(R.array.listOfCategories).toList())
         }
+        binding.spinner.prompt = "Please select one "
+
+        setupListeners()
+
+    }
+
+    private fun setupListeners() {
+
+         binding.spinner.apply {
+             onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                 override fun onItemSelected(
+                     parent: AdapterView<*>?,
+                     view: View?,
+                     position: Int,
+                     id: Long
+                 ) {
+                     var view = parent?.selectedView
+//                 ((view as ConstraintLayout).getViewById(R.id.tv_category_name) as TextView).setText()
+                 }
+
+                 override fun onNothingSelected(parent: AdapterView<*>?) {
+                     binding.spinner.setSelection(0)
+                 }
+
+             }
+         }
+
 
     }
 
