701a953dbde7cd3ff2f2570c9a94f2908023f6b9
anirudh797 <anirudh.kaptiyal77@gmail.com>
2022-07-25 23:29:57 +0530

    July 25 - Refactored CustomAdapter.kt

--- app/src/main/java/com/example/moneytracker/common/CustomAdapter.kt
+++ app/src/main/java/com/example/moneytracker/common/CustomAdapter.kt
@@ -5,11 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.moneytracker.R
 import com.example.moneytracker.databinding.SpinnerListItemBinding
 
 class CustomAdapter(context: Context, resourceId: Int, var list: List<String>) :
     ArrayAdapter<String>(context,resourceId,list) {
+
+    var initialText = "Please Select"
+    var initialTextWasShown : Boolean = false
+
+
+    fun anySelectionMade(spinner: Spinner) : Boolean{
+        return !((spinner.selectedView as? ConstraintLayout)?.getViewById(R.id.tv_category_name) as TextView).text.toString().equals(initialText)
+    }
+
+    fun TextView.getText() : String{
+        return text.toString()
+    }
 
     lateinit var  binding: SpinnerListItemBinding
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -17,15 +32,21 @@ class CustomAdapter(context: Context, resourceId: Int, var list: List<String>) :
     }
 
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return getCustomView(position,convertView,parent)
+
+    }
+
     private fun getCustomView(position: Int, convertView: View?, parent: ViewGroup): View {
 
         var layoutInflater = LayoutInflater.from(context)
         binding = SpinnerListItemBinding.inflate(layoutInflater,parent,false)
         var categoryList = listOf(R.array.listOfCategories)
         binding.apply {
-            tvCategoryName.setText(list[position])
+            tvCategoryName.text = list[position]
         }
         return binding.root
     }
+
 
 }
